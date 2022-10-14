cef98660f8085609bd731f320cc4da581ef54000
kaseungyup <timka0506@gmail.com>
2022-10-14 18:11:17 +0900

    position tracking

--- ros_viz.py
+++ ros_viz.py
@@ -13,8 +13,10 @@ from tf.transformations import quaternion_from_euler
 from timer import Timer
 from visualizerclass import VisualizerClass
 from mahony import Mahony
+from extended_kalman import EKF
 from utils import *
 
+Hz = 100
 apriltag_x = 0.0; apriltag_y = 0.0; apriltag_yaw = 0.0
 
 acc_x = 0.0; acc_y = 0.0; acc_z = 0.0
@@ -66,16 +68,22 @@ if __name__ == '__main__':
     rospy.Subscriber("apriltag_position", String, callback)
     rospy.Subscriber("imu_sensor", String, callback2)
 
-    tmr_plot = Timer(_name='Plot',_HZ=50,_MAX_SEC=np.inf,_VERBOSE=True)
+    tmr_plot = Timer(_name='Plot',_HZ=Hz,_MAX_SEC=np.inf,_VERBOSE=True)
     
     acc_data = []
+    Earth_frame_acc_data = []
     gyro_data = []
+    quat_data = []
+    vel_data = []
+    vel_data.append(np.array([0, 0, 0]))
+    pos_data = []
+    pos_data.append(np.array([0, 0, 0]))
 
     # Set terminator
-    signal.signal(signal.SIGINT, partial(signal_handler,tmr_plot))
+    # signal.signal(signal.SIGINT, partial(signal_handler,tmr_plot))
 
     # Visualizer
-    V = VisualizerClass(name='simple viz',HZ=50)
+    V = VisualizerClass(name='simple viz',HZ=Hz)
     
     # Start the loop 
 
@@ -91,7 +99,7 @@ if __name__ == '__main__':
             x,y, april_yaw = get_position()
             x = 10 * x
             y = 10 * y
-            print(x,y)
+            #print(x,y)
 
             ax, ay, az = get_acc()
             acc_data.append([ax, ay, az])
@@ -100,18 +108,36 @@ if __name__ == '__main__':
             gx, gy, gz = get_gyro()
             gyro_data.append([gx, gy, gz])
 
-            orientation = Mahony(gyr=gyro_data[-40:], acc=acc_data[-40:])
-            q = orientation.Q[-1,:]
-            roll, pitch, yaw = quaternion_to_vector(q[0],q[1],q[2],q[3])
+            orientation_mahony = Mahony(gyr=gyro_data[-40:], acc=acc_data[-40:])
+            q_mahony = orientation_mahony.Q[-1,:]
+
+            Earth_frame_acc = q_rot(q_conj(q_mahony), np.array([ax, ay, az])) - np.array([0,0,9.81])
+            Earth_frame_acc_data.append(np.array(Earth_frame_acc))
+            vel, pos = update_position(Earth_frame_acc[-40:], vel_data[-40:], pos_data[-40:], Hz)
+            #print("vel: ", vel)
+            #print("pos: ", pos)
+
+            roll1, pitch1, yaw1 = quaternion_to_vector(q_mahony[0],q_mahony[1],q_mahony[2],q_mahony[3])
             #yaw = april_yaw
+            #print(roll,pitch,yaw)
 
-            print(roll,pitch,yaw)
-
-            V.append_marker(Quaternion(*quaternion_from_euler(-roll,-pitch,yaw)),Vector3(1,0.3,0.3),x=x,y=y,z=0,frame_id='map',
+            V.append_marker(Quaternion(*quaternion_from_euler(-roll1,-pitch1,yaw1)),Vector3(1,0.3,0.3),x=x,y=y,z=0,frame_id='map',
                 color=ColorRGBA(1.0,0.0,0.0,0.5),marker_type=Marker.ARROW)
+            # text1 = "Mahony\nRoll: %f Pitch: %f Yaw: %f"%(-roll1*R2D,-pitch1*R2D,yaw1*R2D)
+            # V.append_text(x=2,y=0,z=1.3,r=1.0,text=text1,scale=Vector3(0,0,0.3),
+            #     frame_id='map',color=ColorRGBA(1.0,1.0,1.0,0.5))
+
+            # orientation_kalman = EKF(gyr=gyro_data[-40:], acc=acc_data[-40:], mag=None)
+            # q_kalman = orientation_kalman.Q[-1,:]
+            # roll2, pitch2, yaw2 = quaternion_to_vector(q_kalman[0],q_kalman[1],q_kalman[2],q_kalman[3])
+            # V.append_marker(Quaternion(*quaternion_from_euler(-roll2,-pitch2,yaw2)),Vector3(1,0.3,0.3),x=-2,y=0,z=0,frame_id='map',
+            #     color=ColorRGBA(1.0,0.0,0.0,0.5),marker_type=Marker.ARROW)
+            # text2 = "Kalman\nRoll: %f Pitch: %f Yaw: %f"%(-roll2*R2D,-pitch2*R2D,yaw2*R2D)
+            # V.append_text(x=-2,y=0,z=1.3,r=1.0,text=text2,scale=Vector3(0,0,0.3),
+            #     frame_id='map',color=ColorRGBA(1.0,1.0,1.0,0.5))
 
             V.append_text(x=x,y=y,z=1.3,r=1.0,text=text,scale=Vector3(0,0,0.3),
-               frame_id='map',color=ColorRGBA(1.0,1.0,1.0,0.5))
+                frame_id='map',color=ColorRGBA(1.0,1.0,1.0,0.5))
 
             V.publish_markers()
             V.publish_texts()
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import math
-import cv2
-import apriltag
 import numpy as np
-import pyrealsense2 as rs
+
+import numpy as np
+from scipy import signal
+from ahrs.common.orientation import q_prod, q_conj, acc2q, am2q, q2R, q_rot
 
 def quaternion_to_vector(w, x, y, z):
     t0 = +2.0 * (w * x + y * z)
@@ -20,3 +21,78 @@ def quaternion_to_vector(w, x, y, z):
      
     return roll_x, pitch_y, yaw_z
 
+
+def update_position(acc, vel, pos, Hz):
+    threshold = 0.1
+    dt = 1/Hz
+    
+    sample_number = np.shape(acc)[0]
+
+    t_start = 0
+    for t in range(sample_number):
+        at = acc[t]
+        if np.linalg.norm(at) > threshold:
+            t_start = t
+            break
+
+    t_end = 0
+    for t in range(sample_number - 1, -1, -1):
+        at = acc[t]
+        if np.linalg.norm(at - acc[-1]) > threshold:
+            t_end = t
+            break
+
+    an_drift = acc[t_end:].mean(axis=0)
+    if t_start != t_end: an_drift_rate = an_drift / (t_end - t_start)
+
+    for i in range(t_end - t_start):
+        acc[t_start + i] -= (i + 1) * an_drift_rate
+
+    for i in range(sample_number - t_end):
+        acc[t_end + i] -= an_drift
+
+    
+    velocities = []
+    prevt = -1
+    still_phase = False
+
+    v = vel[0]
+    t1 = 0
+    while t1 < sample_number:
+        at = acc[t1, np.newaxis].T
+
+        if np.linalg.norm(at) < threshold:
+            if not still_phase:
+                predict_v = v + at * dt
+
+                v_drift_rate = predict_v / (t1 - prevt)
+                for i in range(t1 - prevt - 1):
+                    velocities[prevt + 1 + i] -= (i + 1) * v_drift_rate.T[0]
+
+                v = np.zeros((3, 1))
+                prevt = t1
+                still_phase = True
+        else:
+            v = v + at * dt
+            still_phase = False
+
+        velocities.append(v.T[0])
+        t1 += 1
+
+    velocities = np.array(velocities)
+
+    positions = []
+    p = pos[0].T
+
+    t2 = 0
+    while t2 < sample_number:
+        at = acc[t2, np.newaxis].T
+        vt = velocities[t2, np.newaxis].T
+
+        p = p + vt * dt + 0.5 * at * dt**2
+        positions.append(p.T[0])
+        t2 += 1
+
+    positions = np.array(positions)
+    print(positions[-1])
+    return velocities[-1], positions[-1]
